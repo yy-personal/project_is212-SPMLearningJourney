f56187d93ee5e7b8f89d1a1ee0947c445e2a9a93
CherylLim <85498185+dracolim@users.noreply.github.com>
2022-10-16 23:22:42 +0800

    Updated app2.py

--- flask/app2.py
+++ flask/app2.py
@@ -455,4 +455,4 @@ def get_learning_journey_skill():
 db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5010, debug=True)g=True)
+    app.run(host='0.0.0.0', port=5010, debug=True)
