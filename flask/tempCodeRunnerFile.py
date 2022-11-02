5e306edca19e8405f77c9dac1732ce4e0ccac97b
yy <yinyao.lim.2020@scis.smu.edu.sg>
2022-11-03 02:24:20 +0800

    Only show unselected course

--- flask/tempCodeRunnerFile.py
+++ flask/tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-@app.route("/skillCourse", methods=['GET'])
-# def read_skillCourse():
-#     skillCourseList = SkillCourse.query.all()
-#     return jsonify(
-#         {
-#             "data": [skillCourse.to_dict()
-#                     for skillCourse in skillCourseList]
-#         }
-#     ), 200
+root
