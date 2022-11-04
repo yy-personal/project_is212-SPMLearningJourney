e21bb71bd03c1c0d7f0bc8ca243cc9a7ccb44760
yy <yinyao.lim.2020@scis.smu.edu.sg>
2022-11-04 20:36:20 +0800

    x
    x

--- flask/tempCodeRunnerFile.py
+++ flask/tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-root
+@app.route("/skillCourse", methods=['GET'])
+# def read_skillCourse():
+#     skillCourseList = SkillCourse.query.all()
+#     return jsonify(
+#         {
+#             "data": [skillCourse.to_dict()
+#                     for skillCourse in skillCourseList]
+#         }
+#     ), 200
